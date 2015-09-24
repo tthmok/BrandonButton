aa7eb30151eab8e632545a19fc73946a7ff1960a
Terrance Mok <tthmok@gmail.com>
2015-09-23 21:39:11 -0600

    Added DOTween and simple test of rotation.

---
+++ Assets/spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using DG.Tweening;
+public class spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.DORotate(new Vector3(0, 0, 361), 10, RotateMode.FastBeyond360)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1);
+	}
+	
+}
