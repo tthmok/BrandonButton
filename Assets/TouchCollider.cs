883c6193fb3d74f2ac088fc07dba3d7f7b79ea48
Terrance Mok <tthmok@gmail.com>
2015-09-23 22:29:09 -0600

    work in progress

---
+++ Assets/TouchCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCollider : MonoBehaviour {
+
+    Collider2D thisCollider2D;
+
+    void Start()
+    {
+        thisCollider2D = GetComponent<Collider2D>();
+    }
+    void Update()
+    {
+        if (Input.G))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (thisCollider2D == Physics2D.OverlapPoint(touchPos))
+            {
+                Messenger.Broadcast("BRANDON_BUTTON_CLICK");
+            }
+        }
+    }
+}
--- Assets/spin.cs
+++ Assets/spin.cs
@@ -6,9 +6,14 @@ public class spin : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Messenger.AddListener("BRANDON_BUTTON_CLICK", StartSpin);
+	}
+
+    public void StartSpin()
+    {
         transform.DORotate(new Vector3(0, 0, 361), 10, RotateMode.FastBeyond360)
             .SetEase(Ease.InOutSine)
             .SetLoops(-1);
-	}
+    }
 	
 }
